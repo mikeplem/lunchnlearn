6d385630245b5feca0054fdbf9fb339cd9363768
Mike Plemmons <mikeplem@users.noreply.github.com>
2018-08-06 19:22:41 -0400

    add more examples for grep and inverting a match while using sed to remove empty lines.  set file permissions on files placed by prep script.

--- prep.sh
+++ prep.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+cp comments_newlines.txt /tmp/comments_newlines.txt
+cp comments.txt /tmp/comments.txt
+
 cp name1.txt /tmp/name1.txt
 cp name2.txt /tmp/name2.txt
 cp name3.txt /tmp/name3.txt
@@ -19,3 +22,13 @@ rm name3.txt
 mkdir -p /tmp/class/file/hidden/here/
 cp findme.txt /tmp/class/file/hidden/here/findme.txt
 rm findme.txt
+
+chmod 444 /tmp/comments_newlines.txt
+chmod 444 /tmp/comments.txt
+chmod 444 /tmp/name1.txt
+chmod 444 /tmp/name2.txt
+chmod 444 /tmp/name3.txt
+chmod 444 /tmp/name1/name1.txt
+chmod 444 /tmp/name2/name2.txt
+chmod 444 /tmp/name3/name3.txt
+chmod 444 /tmp/class/file/hidden/here/findme.txt
